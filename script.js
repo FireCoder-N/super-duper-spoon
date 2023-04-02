24e9f2161a933e354b02acc5cc9b057c9473c872
FireCoder-N <dimnestmanolis@gmail.com>
2023-04-02 15:36:04 +0300

    v2 retry

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 
-// window.addEventListener("resize", () => {
-//     const ratio = parseInt(window.innerWidth) / 1536;
-//     document.querySelector("#Brand").style.fontSize = (28 * ratio) + 'px';
-// });
+window.addEventListener("resize", () => {
+    const ratio = 1536 / parseInt(window.innerWidth);
+    // document.querySelector("#Brand").style.fontSize = (28 * ratio) + 'px';
+    document.querySelector("header").style.height = ratio * 60 + 'px';
+    document.querySelector("header").style.lineHeight = 1.5 + "em";
+});
 
 document.querySelector("#phone").addEventListener('click', () => {
     const phone = "+30-210-1234567";
@@ -22,13 +24,30 @@ document.querySelector("#phone").addEventListener('click', () => {
 
 });
 
-
 document.querySelector("#email").addEventListener('click', () => {
     const email = "thehall.sthena@gmail.com";
     navigator.clipboard.writeText(email);
 
     Toastify({
         text: "Email copied to clipboard!",
+        duration: 2000, //time in ms
+        gravity: "bottom",
+        style: {
+            background: "#3f454f",
+            color: "#ff8e4c"
+        }
+    }).showToast();
+
+});
+
+
+document.querySelector("#facebook").addEventListener('click', () => {
+        window.open('https://www.facebook.com/profile.php?id=100091574676795');
+        window.focus();
+        // window.open('https://www.facebook.com/profile.php?id=100091574676795', 'myNewTab');
+
+    Toastify({
+        text: "Facebook page opened in new tab!",
         duration: 2000, //time in ms
         gravity: "bottom",
         style: {
