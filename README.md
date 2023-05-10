55f60ddf6314982ebd517775bb0634f5e3765ba5
Papaefstathiou Spyridon <126704976+Blue-Cultist@users.noreply.github.com>
2023-05-10 16:52:41 +0300

    Marker functionality, floor placeholders

---
+++ README.md
@@ -0,0 +1,36 @@
+# Technical Problem Report Platform
+
+### ⚠️ This repository is still a work on progress ⚠️
+
+This is a GitHub repository for the university of Patras (department of electrical & computer engineering) course "Internet Programming (ECE_GK802)".
+
+## Project Description
+Our project is a full-stack website titled "Technical Problem Report Platform". We have chosen to create a website for a shopping mall where users can report technical issues they have encountered, and the mall staff can address them. The website is intended for use by users of various groups, such as customers and shopping mall staff .
+
+
+![Mall exterior](https://raw.githubusercontent.com/FireCoder-N/super-duper-spoon/ONA/building%20exterior.png)
+*Image source: https://www.hs3dindia.com/ (edited)*
+
+## User Cases and Privileges
+Our website will have four types of users: customers, staff, store owners/personnel, and administrators. After signing in, users will be presented with an interactive photo of the building's exterior, where they can select the floor where the problem is located. Then, a second screen with the floor plan is opened where the users are asked to pinpoint the problem's location. Finally, they can fill out a form and submit the issue. Admin users will have more privileges, such as marking an issue as resolved. Staff and shop owners/personnel users will also be able to view reports relevant to their place of employment.
+
+## Logo
+![Logo](https://raw.githubusercontent.com/FireCoder-N/super-duper-spoon/ONA/logo.png)
+
+*Logo is created using the website www.freelogodesign.org*
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Handlebars [pending]
+- Node.js [pending]
+- Express [pending]
+- Database (MySQL/SQLite/MongoDB/ MariaDB /...) [pending]
+- Cloud (for hosting) [pending]
+
+
+## Contributors
+- https://github.com/FireCoder-N
+- https://github.com/Blue-Cultist
+
--- script.js
+++ script.js
@@ -1,18 +1,77 @@
 "use strict";
 
+class marker{
+    constructor(id, top, right){
+        this.id = id;
+        this.top = top;
+        this.right = right;
+    }
+}
+
+
+const markers = [
+    new marker("parking", 0.92, 0.75),
+    new marker("ground", 0.87, 0.355),
+    new marker("first", 0.75, 0.535),
+    new marker("second", 0.65, 0.5),
+    new marker("third", 0.55, 0.47)
+];
+
+
+
 window.addEventListener("DOMContentLoaded", () => {
-	document.querySelector("body").style.paddingTop = "70px";
-	document.querySelector("body").style.paddingBottom = "140px";
+    // load markers
+    if (window.location.href.includes("starting_page.html")) {
+        for (let m of markers) {
+            const marker = document.createElement("img");
+            marker.src = "logo_transparent.png";
+            marker.className = "marker";
+            marker.id = m.id;
+            marker.addEventListener("click", () => {
+                window.location.href = m.id + ".html";
+            });
+
+            // style 
+            marker.style.top = m.top*100 + '%';
+            marker.style.right = m.right*100 + '%';
+            marker.style.transform = "translate(-50%, -50%)";
+
+            // add element to DOM
+            document.querySelector("#main").appendChild(marker);
+        }
+    }
+
 });
 
 window.addEventListener("resize", () => {
+
     const ratio = 1536 / parseInt(window.innerWidth);
-    // document.querySelector("#Brand").style.fontSize = (28 * ratio) + 'px';
-    document.querySelector("header").style.height = ratio * 60 + 'px';
-    document.querySelector("header").style.lineHeight = 1.5 + "em";
-	document.querySelector("#main").style.paddingTop = document.querySelector("header").style.height;
-	document.querySelector("#main").style.paddingBottom = document.querySelector("footer").style.height;
+
+    // scale header height
+    document.querySelector("header").style.height = ratio * 70 + 'px';
+    // scale body padding top, so that image is not cut
+    document.querySelector("body").style.paddingTop = ratio * 70 + 'px';
+
+    // scale and position markers
+    if (window.location.href.includes("starting_page.html")) {
+        const container_dim = document.querySelector("#main").getBoundingClientRect();
+
+        for (let m of markers) {
+            const marker = document.querySelector("#" + m.id);
+            marker.style.width = 70 / ratio + 'px';
+            marker.style.height = 70 / ratio + 'px';
+            marker.style.right = container_dim.width*m.right + 'px';
+            marker.style.top = container_dim.height*m.top + 'px';
+        }
+    }
+
 });
+
+
+
+
+
+// ==================================================================================
 
 document.querySelector("#phone").addEventListener('click', () => {
     const phone = "+30-210-1234567";
@@ -46,11 +105,8 @@ document.querySelector("#email").addEventListener('click', () => {
 
 });
 
-
 document.querySelector("#facebook").addEventListener('click', () => {
         window.open('https://www.facebook.com/profile.php?id=100091574676795');
-        window.focus();
-        // window.open('https://www.facebook.com/profile.php?id=100091574676795', 'myNewTab');
 
     Toastify({
         text: "Facebook page opened in new tab!",
@@ -62,13 +118,4 @@ document.querySelector("#facebook").addEventListener('click', () => {
         }
     }).showToast();
 
-});
-
-
-document.querySelector("#parking").addEventListener('click', () => {
-        window.location.href = "parking.html";
-});
-
-document.querySelector("#ground_floor").addEventListener('click', () => {
-        window.location.href = "ground_floor.html";
 });
